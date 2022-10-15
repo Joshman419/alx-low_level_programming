1b963ddd64355c4af496b89d882aeca8f3e269b7
Joshman419 <akinlolujoshua1@gmail.com>
2022-10-14 17:31:19 -0700

    integer sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,12 @@ int main(void)
 	int i;
 	long int ld;
 	long long int lld;
-	float l;
+	float f;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", sizeof(l));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(ld));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(lld));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
+}

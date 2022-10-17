eff79b000944adcbd572d00580006e97326f239c
Joshman419 <akinlolujoshua1@gmail.com>
2022-10-17 13:25:20 -0700

    123456789 with putchar

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- Entry Point
+ *Return: 0 if successful and non success if it fails
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

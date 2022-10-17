c0cbe6765e15c8003aa863a3dacf4110d055418e
Joshman419 <akinlolujoshua1@gmail.com>
2022-10-17 09:52:31 -0700

    putchar use

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- Entry Point
+ *Return: return 0 if Success and non zero if failed
+ */
+int main(void)
+{
+	char i;
+	for (i= 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
+		
+	

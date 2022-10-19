7dd774561ed033921ad8bbe34da30e01820f4649
Joshman419 <akinlolujoshua1@gmail.com>
2022-10-19 15:40:21 -0700

    10 time lower case

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet_10x - print alphabet 10 times, in lower case
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 	int count = 0;
 	char alpha;
@@ -11,9 +11,7 @@ void print_alphabet_x10(void)
 	while (count++ <= 9)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
-		{
 			_putchar(alpha);
-		}
 		_putchar('\n');
 	}
 }
